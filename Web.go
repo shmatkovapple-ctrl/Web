bacaf74e4b27e485e5bc4e9dcebb0c35e4466ad2
shmat <shmatkov.apple@gmail.com>
2025-10-29 22:14:18 +0500

    Певроначальный коммит

--- Web.go
+++ Web.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	if err := http.ListenAndServe(":8000", http.FileServer(http.Dir("."))); err != nil {
+		fmt.Println("Произошла ошибка при запуске сервера", err)
+	}
 }
